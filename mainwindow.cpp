4c99594e7e59651ba404d94b99770f48c445cd54
fdf <shiwm@home.com>
2019-11-11 23:09:09 +0800

    新增菜单

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,17 @@
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    resize(800,600);
+    QMenuBar *mBar = menuBar();
+    QMenu *pFile = mBar->addMenu("File");
+    QAction *pNew = pFile->addAction("New");
+    pFile->addSeparator();
+    QAction *pExit = pFile->addAction("Exit");
 }
 
 MainWindow::~MainWindow()
